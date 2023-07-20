c40a70ed9a8927f36de4b8031df5f3657fba722a
Salvador <45272829+salvariable@users.noreply.github.com>
2023-07-20 15:24:51 -0600

    Add Git answers

---
+++ answers.js
@@ -0,0 +1,15 @@
+// The purpose of the feature branch is to isolate own changes from a shared workspace, allowing to merge those changes later.
+// A new Git repository is set up with the 'git init' command.
+// The main branch is the main hub where all changes are met.
+// Using the 'git branch' command.
+// Using the 'git checkout' command.
+// By commiting changes in preparation for a pull request.
+// When in main branch: 'git merge feature'.
+// Attend to the merge conflicts indicators so the conflicted lines can be addressed.
+// To prevent any previous or current changes were not affected.
+// Account the safest way to prevent and resolve merge conflicts; also ensure appropriate branch naming.
+// Using the command 'git branch -d feature'.
+// Ensure other members of the team can comment on the submitted changes and approve them before merging.
+// Using the command 'git push feature remote <name-of-repo>'.
+// Using the model 'feature/name-of-branch'.
+// By pull-rebasing frequently from main.
